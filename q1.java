edbb48f290c18caaedbbf00689bbd1d0a0c4b73c
minhhoang2703 <s3924716@rmit.edu.vn>
2022-12-13 15:11:01 +0700

    why me

---
+++ q1.java
@@ -0,0 +1,16 @@
+package q1;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("src/users.txt"));
+        while(sc.hasNext()){
+            String name = sc.next();
+            String address = sc.next();
+
+        }
+
+    }
+}
